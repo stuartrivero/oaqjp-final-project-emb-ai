c01abe442345bba3e162f381401fed46ed395928
stuartrivero <stuart.rivero@thinkto.be>
2026-02-20 16:26:13 -0500

    task 4

---
+++ server.py
@@ -0,0 +1,6 @@
+
+from EmotionDetection.emotion_detection import emotion_detector
+
+if __name__ == "__main__":
+    # print(emotion_detector("I love this new technology."))
+    print(emotion_detector("I hate working long hours"))
